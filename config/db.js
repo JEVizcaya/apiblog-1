a9fde8f14f910c3b95cc12744e415f0abc6c1703
JEVizcaya <jevizcaya@hotmail.com>
2025-05-06 17:26:22 +0200

    subiendo render

--- config/db.js
+++ config/db.js
@@ -1,19 +1,18 @@
-const mysql = require('mysql2');
+const { Pool } = require('pg');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Necesario para Render
+  },
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error('Error al conectar a la base de datos:', err);
-    process.exit(1); // Salir si hay un error
-  }
-  console.log('Conexión a la base de datos establecida');
-});
+pool.connect()
+  .then(() => console.log('Conexión a la base de datos PostgreSQL establecida'))
+  .catch(err => {
+    console.error('Error al conectar a la base de datos PostgreSQL:', err);
+    process.exit(1);
+  });
 
-module.exports = db;
+module.exports = pool;
